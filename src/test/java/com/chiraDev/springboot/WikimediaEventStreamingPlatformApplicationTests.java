a4f2415f5112dfba77945c02a998ebd3c99a2f47
CR Hapuarachchi <112224823+0019-KDU@users.noreply.github.com>
2024-10-08 13:44:22 +0530

    Wikimedia Producer Spring Boot Project Setup            Configure Wikimedia Producer and Create a Topi     Wikimedia Producer Implementation                   Run Wikimedia Producer

---
+++ src/test/java/com/chiraDev/springboot/WikimediaEventStreamingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiraDev.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaEventStreamingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
