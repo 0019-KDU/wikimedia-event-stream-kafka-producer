37dedb1103f1980cb7fb5855e46badb4e8c3f007
CR Hapuarachchi <112224823+0019-KDU@users.noreply.github.com>
2024-10-08 13:44:22 +0530

    create the multi module maven project

---
+++ src/main/java/com/chiraDev/springboot/WikimediaEventStreamingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.chiraDev.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaEventStreamingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaEventStreamingPlatformApplication.class, args);
+	}
+
+}
